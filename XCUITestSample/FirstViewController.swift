e764efa7218ef0461d1076370964c264c1ee04cc
ichiko <ichigo_sky@hotmail.com>
2015-11-15 11:11:55 +0900

    Implement First view.

---
+++ XCUITestSample/FirstViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FirstViewController.swift
+//  XCUITestSample
+//
+//  Created by ichiko on 2015/11/15.
+//  Copyright © 2015年 ichiko_revjune. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var calculateLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func clickCalculateButton(sender: AnyObject) {
+        calculateLabel.text = doubleNumber(textInput.text)
+    }
+
+    private func doubleNumber(numberText: String?) -> String {
+        guard numberText != nil, let result = Int(numberText!) else {
+            return "計算できません"
+        }
+        return "\(result * 2)"
+    }
+
+    @IBAction func changeProgressSlider(sender: UISlider) {
+        let display = NSString(format: "%.1f", sender.value) as String
+        progressLabel.text = display
+    }
+}
+
