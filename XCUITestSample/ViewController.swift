ce0abfb9c54d425f6cf2b7d2725741feb2e58175
ichiko <ichigo_sky@hotmail.com>
2015-11-15 10:51:42 +0900

    Create Project.

---
+++ XCUITestSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCUITestSample
+//
+//  Created by ichiko on 2015/11/15.
+//  Copyright © 2015年 ichiko_revjune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
